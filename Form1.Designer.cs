b1c4809acca2e9aca071faac4ffbcd48487a63c6
francisowentabuniar <francisowentabuniar@SMU-0132>
2024-12-05 10:50:07 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace OOP_Registration_from
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnsubmit = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContactno = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.t = new System.Windows.Forms.Panel();
+            this.dgvregistration = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.t.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvregistration)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnsubmit);
+            this.panel1.Controls.Add(this.btncancel);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtContactno);
+            this.panel1.Controls.Add(this.txtAddress);
+            this.panel1.Controls.Add(this.txtLastname);
+            this.panel1.Controls.Add(this.txtFirstname);
+            this.panel1.Location = new System.Drawing.Point(34, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(462, 355);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnsubmit
+            // 
+            this.btnsubmit.Location = new System.Drawing.Point(376, 305);
+            this.btnsubmit.Name = "btnsubmit";
+            this.btnsubmit.Size = new System.Drawing.Size(74, 27);
+            this.btnsubmit.TabIndex = 11;
+            this.btnsubmit.Text = "Submit";
+            this.btnsubmit.UseVisualStyleBackColor = true;
+            this.btnsubmit.Click += new System.EventHandler(this.btnsubmit_Click_1);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(296, 305);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(74, 27);
+            this.btncancel.TabIndex = 10;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Contact number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "NO,street, Barangay province";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lastname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Full Name";
+            // 
+            // txtContactno
+            // 
+            this.txtContactno.Location = new System.Drawing.Point(34, 274);
+            this.txtContactno.Name = "txtContactno";
+            this.txtContactno.Size = new System.Drawing.Size(124, 20);
+            this.txtContactno.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(34, 185);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(343, 20);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(253, 76);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(124, 20);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(34, 76);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(124, 20);
+            this.txtFirstname.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(30, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(246, 20);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Conference Registration form";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(69, 43);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(208, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Please fill in the  form below. We\'ll contact ";
+            // 
+            // t
+            // 
+            this.t.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.t.Controls.Add(this.dgvregistration);
+            this.t.Location = new System.Drawing.Point(520, 56);
+            this.t.Name = "t";
+            this.t.Size = new System.Drawing.Size(679, 329);
+            this.t.TabIndex = 3;
+            // 
+            // dgvregistration
+            // 
+            this.dgvregistration.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvregistration.Location = new System.Drawing.Point(3, 4);
+            this.dgvregistration.Name = "dgvregistration";
+            this.dgvregistration.Size = new System.Drawing.Size(673, 326);
+            this.dgvregistration.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 448);
+            this.Controls.Add(this.t);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.t.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvregistration)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContactno;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.Button btnsubmit;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel t;
+        private System.Windows.Forms.DataGridView dgvregistration;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Registration_from
+{
+    public partial class Form1 : Form
+    {
+        DBConnect connect = new DBConnect();
+        Registration reg = new Registration();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            connect.OpenDB();
+            DisplayRegistration();
+        }
+
+        private void btnsubmit_Click_1(object sender, EventArgs e)
+        {
+
+            try
+            {
+                reg.firstname = txtFirstname.Text;
+                reg.lastname = txtLastname.Text;
+                reg.address = txtAddress.Text;
+                reg.contactno = txtContactno.Text;
+                connect.InsertRegistration(reg);
+                MessageBox.Show("You have submitted successfully");
+                DisplayRegistration();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void DisplayRegistration()
+        {  
+           connect.OpenDB();
+            DataTable dt = new DataTable();
+            dt = connect.ReadRegistrationrRecord();
+            dgvregistration.DataSource = dt;
+
+        }
+
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
